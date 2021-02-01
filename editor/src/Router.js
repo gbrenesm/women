1e8c8103a14be42f69cf4c307a04fc7f1456715f
Guadalupe Brenes <brenesmesa@gmail.com>
2021-02-01 16:16:51 -0600

    Intial configs and adding home page

---
+++ editor/src/Router.js
@@ -0,0 +1,14 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home'
+
+const Router = () => {
+  return(
+    <BrowserRouter>
+      <Switch>
+        <Route component = { Home } path='/' />
+      </Switch>
+    </BrowserRouter>
+  )
+};
+
+export default Router;
