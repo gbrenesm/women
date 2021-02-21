3dc0c7dbc697cdc6bd9b40c87834d6dc58bdb384
Guadalupe Brenes <brenesmesa@gmail.com>
2021-02-21 14:12:06 -0600

    Styles for form

--- editor/src/components/AddRecordForm.js
+++ editor/src/components/AddRecordForm.js
@@ -47,25 +47,50 @@ const AddRecordForm = ({ setNewNote, womanId, setForm }) => {
   }
 
   return (
-    <form onSubmit={handleSubmit} className="form record-form">
-      <label htmlFor="name" >Título de la nota</label>
-      <input type="text" name="name" id="name"  placeholder="Título" {...nameInput} />
-      <label htmlFor="newspaper" >Periódico</label>
-      <input type="text" name="newspaper" id="newspaper"  placeholder="Nombre de la publicación" {...newspaperInput}/>
-      <label htmlFor="publicationDate" >Fecha de publicación</label>
-      <input type="date" name="publicationDate" id="publicationDate" {...publicationDateInput} />
-      <label htmlFor="page" >Página(s)</label>
-      <input type="text" name="page" id="page" placeholder="2, 5 y 7" {...pageInput}/>
-      <label htmlFor="publicationPlace" >Lugar de publicación</label>
-      <input type="text" name="publicationPlace" id="publicationPlace" placeholder="Ciudad o país"  {...publicationPlaceInput} />
-      <label htmlFor="transcription" >Transcripción</label>
-      <textarea name="transcription" id="transcription" placeholder="Transcripción"  {...transcriptionInput} />
-      <label htmlFor="file" >Imagen de la nota</label>
-      <input type="file" name="file" id="file" onChange={uploadFile}/>
-      <label htmlFor="url" >URL de la nota</label>
-      <input type="text" name="url" id="url" placeholder="https://..." {...urlInput}/>
-      <button type="submit">Crear registro</button>
+    <div className="form record-form">
+    <h3>Añadir nota de periódico</h3>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name" >Título de la nota:</label>
+        <input type="text" name="name" id="name"  placeholder="Título" className="long" {...nameInput} />
+      </div>
+      <div>
+        <label htmlFor="newspaper" >Periódico:</label>
+        <input type="text" name="newspaper" id="newspaper" placeholder="Nombre de la publicación" className="long" {...newspaperInput}/>
+      </div>
+      <div>
+        <div>
+          <label htmlFor="publicationDate" >Fecha de publicación:</label>
+          <input type="date" name="publicationDate" id="publicationDate" {...publicationDateInput} />
+        </div>
+        <div>
+          <label htmlFor="page" >Página(s):</label>
+          <input type="text" name="page" id="page" placeholder="2, 5 y 7" {...pageInput}/>
+        </div>
+      </div>
+      <div>
+        <label htmlFor="publicationPlace" >Lugar de publicación:</label>
+        <input type="text" name="publicationPlace" id="publicationPlace" placeholder="Ciudad o país"  {...publicationPlaceInput} />
+      </div>
+      <div>
+        <label htmlFor="transcription" >Transcripción:</label>
+        <br/>
+        <textarea name="transcription" id="transcription" placeholder="Transcripción"  {...transcriptionInput} />
+      </div>
+      <div>
+        <label htmlFor="file" >Imagen de la nota</label>
+        <input type="file" name="file" id="file" onChange={uploadFile}/>
+      </div>
+      <div>
+        <label htmlFor="url" >URL de la nota:</label>
+        <input type="text" name="url" id="url" placeholder="https://..." className="long"  {...urlInput}/>
+      </div>
+      <div className="button">
+        <button type="submit">Crear registro</button>
+        <button onClick={() => setForm(false)} >Cancelar</button>
+      </div>
     </form>
+    </div>
   )
 }
 
